34791c58fc32d3ecc07a101d766e15b1b42dc197
teisiadrian <teisiadrian@yahoo.com>
2024-09-18 14:01:57 +0000

    Initial commit - Adding multi-app project

---
+++ app1/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('App1 connected to Database');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from App1!');
+});
+
+app.listen(3000, () => {
+  console.log('App1 running on port 3000');
+});
